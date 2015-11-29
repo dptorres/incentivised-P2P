6fa6a27e07c9d0c80b7369a9b6bd26517b9ded6c
Danah Veronica Torres <danaveronicatorres@gmail.com>
2015-11-29 22:42:04 +0800

    Initial project setup.

---
+++ src/IncentiveProtocol.java
@@ -0,0 +1,13 @@
+import peersim.cdsim.CDProtocol;
+import peersim.core.Node;
+
+/**
+ * Created by Danah Veronica Torres on 11/29/15.
+ */
+public class IncentiveProtocol implements CDProtocol {
+
+    @Override
+    public void nextCycle(Node node, int i) {
+
+    }
+}
